51b8f38dd5447b2db7060736fa05fa0a15a28b8c
bahermohamed1 <145294284+bahermohamed1@users.noreply.github.com>
2023-09-17 23:39:00 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "wro",
+  "version": "1.0.0",
+  "description": "",
+  "main": "main.js",
+  "scripts": {
+    "start": "electron ."
+  },
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "webpack": "^5.88.2",
+    "webpack-cli": "^5.1.4"
+  },
+  "dependencies": {
+    "electron": "^26.2.1"
+  }
+}
